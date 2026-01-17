905a3b2232537bae31d98a943b7355db548526b9
YusufBro01 <ibrohimovyusufbro@gmail.com>
2026-01-17 17:24:16 +0500

    Render uchun port qo'shildi

--- index.js
+++ index.js
@@ -3903,3 +3903,11 @@ bot.action('stop_test', async (ctx) => {
 });
 
 bot.launch();
+
+
+
+const http = require('http');
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Bot ishlayapti!');
+}).listen(process.env.PORT || 3000);
